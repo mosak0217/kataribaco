c4be9c727b0a1d076afd121e05afac8ba94bf6bc
mosak <mosak0217@gmail.com>
2022-08-02 17:38:11 +0900

    first commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,33 @@
 'use strict';
+
+var swiper = new Swiper('.swiper-container', {
+	navigation: {
+	nextButton: '.swiper-button-next',
+  prevButton: '.swiper-button-prev',
+
+	},
+  slidesPerView: 1,
+  slideToClickedSlide: true,
+  loop: true,
+  dots: true,
+  infinite: true,
+  fade: true,
+  speed: 500,
+  slidesToShow: 1,
+  pauseOnHover: true,
+  accessibility: true,
+  swipe: true,
+	autoplay: {
+		delay: 3000,
+		disableOnInteraction: true
+	},
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true,
+  },
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
+
